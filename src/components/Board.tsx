6d931e777b4b51c7e378869c7d2b4362545fe56b
celuu <christine.e.luu@gmail.com>
2024-04-15 20:23:03 -0700

    Finished tic tac toe.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./board.css";
 import Square from "./Square";
+import { createSecureContext } from "tls";
 
 const Board: React.FC = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
@@ -14,8 +15,32 @@ const Board: React.FC = () => {
     }
   }
 
+  const checkWinner = (squares: Array<number>) => {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+          squares[a] &&
+          squares[a] === squares[b] &&
+          squares[a] === squares[c]
+        ) {
+          return squares[a];
+        }
+      }
+      return null;
+  }
+
   const handleClick = (index: number) => {
-    if(squares[index]) return;
+    if(squares[index] || checkWinner(squares)) return;
     let squaresCopy = [...squares];
     squaresCopy[index] = currentPlayer;
     setSquares(squaresCopy)
@@ -27,6 +52,7 @@ const Board: React.FC = () => {
       <button onClick={() => setSquares(Array(9).fill(null))}>
         Start Game
       </button>
+      <div>{checkWinner(squares)} is the Winner!!!</div>
       <text>You are: {currentPlayer} </text>
       <div className="outer-container">
         <div className="grid-container">
